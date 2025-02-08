ee4c950cea56e0fa2857028c29af1f5f575dff47
Ayala-Yagelnik <a83245064@gmail.com>
2025-02-09 00:12:57 +0200

    3

--- ToDoClient/src/components/Layout.js
+++ ToDoClient/src/components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
     <>
       <AppHeader />
       <Container maxWidth="lg">
-        <Box component="main" sx={{ flexGrow: 1 ,mt:30,mb:30}}>
+        <Box component="main" sx={{ flexGrow: 1 ,mt:20,mb:20}}>
           {children}
         </Box>
       </Container>
