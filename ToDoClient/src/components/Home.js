44464cfb0c8b9c5155a9d46dbeebaa4ef9166103
Ayala-Yagelnik <a83245064@gmail.com>
2025-02-09 01:11:42 +0200

    change margin of home

--- ToDoClient/src/components/Home.js
+++ ToDoClient/src/components/Home.js
@@ -12,13 +12,12 @@ const Home = () => {
     <Container
       maxWidth="lg"
       sx={{
-        height: '100vh',
+        height: '80vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         textAlign: 'center',
-        padding: 4,
       }}
     >
         <TaskIcon sx={{ fontSize: 80, color: 'primary.main' }} />
