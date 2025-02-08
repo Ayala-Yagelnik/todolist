437e11099221629cbac164396acb1d2ed680ba0f
Ayala-Yagelnik <a83245064@gmail.com>
2025-02-08 20:05:49 +0200

    Fix import path for theme in index.js

--- ToDoClient/src/index.js
+++ ToDoClient/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from './compponents/theme';
+import theme from './components/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
